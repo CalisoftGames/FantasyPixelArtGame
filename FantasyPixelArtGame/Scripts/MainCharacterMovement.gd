558d2d1131ea5859bbe5360bf9c8b5d1c55e6c91
JosephRDoherty <109316023+JosephRDoherty@users.noreply.github.com>
2022-08-08 00:10:35 -0700

    Reorganize Attempt

---
+++ FantasyPixelArtGame/Scripts/MainCharacterMovement.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export var speed := 50.0
+
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+# state_machine.travel("walkHorizontal_right")
+
+func _physics_process(delta: float) -> void:
+	var input_vector := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+	var move_direction := input_vector.normalized()
+	move_and_slide(speed * move_direction)
