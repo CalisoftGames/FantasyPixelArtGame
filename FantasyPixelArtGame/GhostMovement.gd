ac42eb479cc91edf906e70a72c37ac726aff8620
JosephRDoherty <109316023+JosephRDoherty@users.noreply.github.com>
2022-08-07 17:47:41 -0700

    Add character movement

---
+++ FantasyPixelArtGame/GhostMovement.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+export var speed := 50.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	var input_vector := Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+	var move_direction := input_vector.normalized()
+	move_and_slide(speed * move_direction)
